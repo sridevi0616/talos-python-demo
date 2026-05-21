c90f4bee1f702ae780dc7f1ad0c927af7629a7bf
sridevi0616 <srideviashok1209@gmail.com>
2026-05-21 14:19:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Talos GitOps demo"
+
+@app.route("/health")
+def health():
+    return {"status":"healthy"}
+
+if __name__=="__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
