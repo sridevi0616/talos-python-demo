42e87bf20a7ef8ac9641a8181a2324e24c74a679
sridevi0616 <srideviashok1209@gmail.com>
2026-05-22 11:51:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello GitOps Auto Deployment 1"
+    return "Hello GitOps Auto Deployment after adding SCA"
 
-@app.route("/health")
+
+@app.route("/health") 
 def health():
-    return {"status":"healthy"}
+    return { "status":"healthy "}
+
 
 if __name__=="__main__":
     app.run(
