4a00e09a2f792352ce6415bb4a1eef9c39ff66a9
sridevi0616 <srideviashok1209@gmail.com>
2026-05-22 15:33:43 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ def data():
         "service": "backend",
         "hostname": socket.gethostname(),
         "redis_hits": count,
-        "text": "HII from backend"
+        "text": "HII from backend after build changes"
     }
 
 @app.route("/health")
