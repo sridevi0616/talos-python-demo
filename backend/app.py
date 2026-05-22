3c8b3c46d95da6a8daadefb472cfd5c8c78b8f86
sridevi0616 <srideviashok1209@gmail.com>
2026-05-22 14:07:22 +0530

    Create app.py for backend

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import socket
+import redis
+
+app = Flask(__name__)
+
+r = redis.Redis(host='redis', port=6379)
+
+@app.route("/api/data")
+def data():
+
+    count = r.incr("hits")
+
+    return {
+        "service": "backend",
+        "hostname": socket.gethostname(),
+        "redis_hits": count
+    }
+
+@app.route("/health")
+def health():
+    return {"status":"healthy"}
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
