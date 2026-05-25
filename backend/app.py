4ba149051e4f7e4f63334c926f00aa84431fb909
sridevi0616 <srideviashok1209@gmail.com>
2026-05-25 21:50:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ def data():
         "service": "backend",
         "hostname": socket.gethostname(),
         "redis_hits": count,
-        "text": "Hello from backend"
+        "text": "Hello from GITOPS"
     }
 
 @app.route("/health")
